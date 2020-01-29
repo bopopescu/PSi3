9535e8bbc963afd39c78dfcaca23c37cdd5dbe54
Mateusz <mateusz.suchodolski@op.pl>
2020-01-30 00:47:07 +0100

    projekt

--- portal_towarzyski/portal/uzytkownicy/serializers.py
+++ portal_towarzyski/portal/uzytkownicy/serializers.py
@@ -3,24 +3,24 @@ from django.contrib.auth.models import User
 from .models import Zainteresowania, Dane_uzytkownika, Znajomi, Znajomi_has_zainteresowania_uzytkownik
 from rest_framework import serializers
 
-class ZainteresowaniaSerializer(serializers.Serializer):
+class ZainteresowaniaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Zainteresowania
-        fields = '_all_'
+        fields = '__all__'
 
-class Dane_uzytkownikaSerializer(serializers.Serializer):
+class Dane_uzytkownikaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dane_uzytkownika
-        fields = '_all_'
-class ZnajomiSerializer(serializers.Serializer):
+        fields = '__all__'
+class ZnajomiSerializer(serializers.ModelSerializer):
     class Meta:
         model = Znajomi
-        fields = '_all_'
-class Znajomi_has_zainteresowania_uzytkownikSerializer(serializers.Serializer):
+        fields = '__all__'
+class Znajomi_has_zainteresowania_uzytkownikSerializer(serializers.ModelSerializer):
     class Meta:
         model = Znajomi_has_zainteresowania_uzytkownik
-        fields = '_all_'
-class UserSerializer(serializers.Serializer):
+        fields = '__all__'
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '_all_'
+        fields = '__all__'
